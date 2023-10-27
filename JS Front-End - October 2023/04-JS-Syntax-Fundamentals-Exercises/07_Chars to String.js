7821d733b65bb905bddf03d45f54767b85b65fa7
aChaushev <119160647+aChaushev@users.noreply.github.com>
2023-10-27 09:09:44 +0300

    07_Chars to String

---
+++ JS Front-End - October 2023/04-JS-Syntax-Fundamentals-Exercises/07_Chars to String.js
@@ -0,0 +1,9 @@
+function solve(char1, char2, char3){
+    // console.log(char1 + char2 + char3)
+    console.log(`${char1}${char2}${char3}`)
+}
+
+solve('a','b','c');
+solve('%','2','o');
+solve('1','5','p');
+
