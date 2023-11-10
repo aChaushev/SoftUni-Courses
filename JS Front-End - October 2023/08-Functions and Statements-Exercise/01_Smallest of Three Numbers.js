5c47ebd22f9046072a48554df88c9b8288280d38
aChaushev <119160647+aChaushev@users.noreply.github.com>
2023-11-10 19:06:19 +0200

    01_Smallest of Three Numbers

---
+++ JS Front-End - October 2023/08-Functions and Statements-Exercise/01_Smallest of Three Numbers.js
@@ -0,0 +1,8 @@
+function smallestNum(num1, num2, num3) {
+    console.log(Math.min(num1, num2, num3));
+}
+
+smallestNum(2, 5, 3);
+smallestNum(600, 342, 123);
+smallestNum(25, 21, 4);
+smallestNum(2, 2, 2);
