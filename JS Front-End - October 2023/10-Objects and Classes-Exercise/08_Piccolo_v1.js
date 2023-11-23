06e6d9fbe079d91e1c95c2522db90b4f0051c596
aChaushev <119160647+aChaushev@users.noreply.github.com>
2023-11-23 10:14:07 +0200

    08_Piccolo_v1

---
+++ JS Front-End - October 2023/10-Objects and Classes-Exercise/08_Piccolo_v1.js
@@ -0,0 +1,39 @@
+function solve(input) {
+  let cars = [];
+  input.forEach((command) => {
+      
+    if (command.includes("IN")) {
+      const [_, car] = command.split("IN, ");
+      if (!cars.includes(car)) {
+        cars.push(car);
+      }  
+    } else if (command.includes("OUT")) {
+      const [_, car] = command.split("OUT, ");
+      if (cars.includes(car)) {
+        cars.splice(cars.indexOf(car), 1);
+      }
+    }
+  });
+
+  if (cars.length === 0) {
+    console.log("Parking Lot is Empty");
+  } else {
+      cars.sort((a, b) => a.localeCompare(b))
+      .forEach((car) => console.log(car));  
+  }
+}
+
+solve([
+  "IN, CA2844AA",
+  "IN, CA1234TA",
+  "OUT, CA2844AA",
+  "IN, CA9999TT",
+  "IN, CA2866HI",
+  "OUT, CA1234TA",
+  "IN, CA2844AA",
+  "OUT, CA2866HI",
+  "IN, CA9876HH",
+  "IN, CA2822UU",
+]);
+
+solve(["IN, CA2844AA", "IN, CA1234TA", "OUT, CA2844AA", "OUT, CA1234TA"]);
