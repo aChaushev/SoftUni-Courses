c3cd032d0ca45b53587ecf7f7e69bcb51d5a42fa
aChaushev <119160647+aChaushev@users.noreply.github.com>
2023-12-22 20:44:50 +0200

    03-Data Definition and Datatypes-Lab

---
+++ MySQL - January 2024/03-04-Databases Introduction. Data Definition and Datatypes/03-Data Definition and Datatypes-Lab.sql
@@ -0,0 +1,45 @@
+-- 01.	Create Tables	
+
+CREATE DATABASE `gamebar`;
+
+CREATE TABLE `gamebar`.`employees` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(100) NOT NULL,
+	`last_name` VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE `gamebar`.`categories` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL
+);
+ 
+CREATE TABLE `gamebar`.`products` (
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `category_id`INT NOT NULL
+);
+
+-- 2.	 Insert Data in Tables 
+
+USE `gamebar`;
+
+INSERT INTO `employees`(`first_name`, `last_name`) VALUES ("Dasha", "Masha");
+INSERT INTO `employees`(`first_name`, `last_name`) VALUES
+("Pesho", "Gosho"),
+("Sasho", "Tosho");
+
+-- 3. Alter Tables 
+
+ALTER TABLE `employees`
+ADD COLUMN `middle_name` VARCHAR(100);
+
+-- 4. Adding Constraints
+ALTER TABLE `products`
+ADD CONSTRAINT fk_products_categories
+FOREIGN KEY (`category_id`)
+REFERENCES `categories` (`id`);
+
+-- 5. Modifying Columns 
+ALTER TABLE `employees`
+MODIFY COLUMN `middle_name` VARCHAR(100) NOT NULL;
+
