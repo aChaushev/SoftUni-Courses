d3c6acfd8c7f5e06d56344c16a27a87a5abd4356
aChaushev <119160647+aChaushev@users.noreply.github.com>
2024-02-02 21:05:47 +0200

    _01_CarInfo

---
+++ Java Advanced - May 2023/_13_DefiningClasses_Lab/_01_CarInfo/Car.java
@@ -0,0 +1,45 @@
+package _13_DefiningClasses_Lab._01_CarInfo;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int horsepower;
+
+    public Car(String brand, String model, int horsepower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsepower = horsepower;
+    }
+
+    public Car(String brand) {
+        this(brand, "unknown", -1);
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setHorsepower(int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public String toString() {
+        return String.format("The car is: %s %s - %d HP.", brand, model, horsepower);
+    }
+}
