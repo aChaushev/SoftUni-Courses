9edc1c9efb864f720b7b73acb8510151ca47f1c2
aChaushev <119160647+aChaushev@users.noreply.github.com>
2024-02-03 08:57:34 +0200

    _02_Constructors

---
+++ Java Advanced - May 2023/_13_DefiningClasses_Lab/_02_Constructors/Main.java
@@ -0,0 +1,34 @@
+package _13_DefiningClasses_Lab._02_Constructors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Car> cars = new ArrayList<>();
+
+        while (n-- > 0){
+            String[] data = scanner.nextLine().split("\\s+");
+
+//            Car car = data.length == 1
+//                    ? new Car(data[0])
+//                    : new Car(data[0], data[1], Integer.parseInt(data[2]));
+            Car car;
+            if(data.length == 1){
+                car = new Car(data[0]);
+            } else {
+                car = new Car(data[0], data[1], Integer.parseInt(data[2]));
+            }
+            cars.add(car);
+
+        }
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+    }
+}
