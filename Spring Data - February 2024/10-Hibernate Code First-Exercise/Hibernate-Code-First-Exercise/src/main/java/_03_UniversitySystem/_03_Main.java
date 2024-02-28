5698e61271a89c328fc6d7c104629d45ab4be4c5
aChaushev <119160647+aChaushev@users.noreply.github.com>
2024-02-28 20:45:12 +0200

    _03_UniversitySystem

---
+++ Spring Data - February 2024/10-Hibernate Code First-Exercise/Hibernate-Code-First-Exercise/src/main/java/_03_UniversitySystem/_03_Main.java
@@ -0,0 +1,23 @@
+package _03_UniversitySystem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class _03_Main {
+    public static void main(String[] args) {
+        EntityManagerFactory factory =
+                Persistence.createEntityManagerFactory("CodeFirstEx");
+
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+
+
+
+
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+}
